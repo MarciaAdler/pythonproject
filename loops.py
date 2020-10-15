a4c86f5b58d000f3a28a2df0e5a3495a13984a40
Marcia <marciaadler718@gmail.com>
2020-10-15 11:28:33 -0400

    loops.py wokr

--- loops.py
+++ loops.py
@@ -1,5 +1,33 @@
 # A for loop is used for iterating over a sequence (that is either a list, a tuple, a dictionary, a set, or a string).
 
+people = ['John','Will','Janet','Karen']
+# simple for loop
+# for person in people:
+#     print('current persion: ', person)
 
+# Break out
+# for person in people:
+#     print('current persion: ', person)
+#     if person == 'Janet':
+#         break
+
+#continue 
+# for person in people:
+#     if person == 'Janet':
+#         continue
+#     print('current person: ',person)
+
+# Range
+# for i in range(len(people)):
+#     print('Current Person: ', people[i])
+
+# for loop up to but not including second number
+# for i in range(0,10):
+#     print('Number ', i)
 
 # While loops execute a set of statements as long as a condition is true.
+count = 0
+
+while count <= 10:
+    print('Count: ', count)
+    count += 1
